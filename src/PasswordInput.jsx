2ced70244850c7492c48f4ea4e5a5aef99096505
Veselin Metodiev <v.metodiev1718@gmail.com>
2023-09-18 08:59:23 +0300

    auto resizable height input area

--- src/PasswordInput.jsx
+++ src/PasswordInput.jsx
@@ -1,15 +1,27 @@
-const PasswordInput = ({passwordHasChanged}) => {
+import useAutosizeTextArea from "./hooks/useAutosizeTextArea";
+import { useRef } from "react";
+const PasswordInput = ({ passwordHasChanged }) => {
+  const value = useRef("");
+  const textAreaRef = useRef(null);
+  const handleChange = (e) => {
+    passwordHasChanged(e.target.value);
+    value.current = e.target.value;
+  };
 
-    const handleInputChange = (e) => {
-        passwordHasChanged(e.target.value);
-      };
+  useAutosizeTextArea(textAreaRef.current, value.current);
 
-    return (
-      <div className="centered-input-container">
-        <input type="text" className="centered-input" placeholder="Enter your password..." 
-        onChange={handleInputChange} />
-      </div>
-    );
-  }
+  return (
+    <div className="centered-input-container">
+      <textarea
+        id="review-text"
+        className="centered-input"
+        onChange={handleChange}
+        placeholder="Enter your password..."
+        ref={textAreaRef}
+        rows={1}
+      />
+    </div>
+  );
+};
 
-  export default PasswordInput;
+export default PasswordInput;
